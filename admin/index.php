e36da8aceee199b8a3e5aa9f4592d6501f2e92f7
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-04-20 23:54:41 -0400

    stylized dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,7 @@
 <?php 
 
-//To Do: Login Needed
-    require_once('scripts/config.php');
+    require_once('scripts/config.php'); // need to be logged in to view this page
     confirm_logged_in();
-
 
 ?>
 
@@ -13,9 +11,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Welcome to your admin panel</title>
 </head>
 <body>
+
+<div class="dashboard">
 
     <h1>Admin Dashboard</h1>
     <h3>Welcome <?php echo $_SESSION['user_name'];?></h3>
@@ -26,11 +27,13 @@
             <li><a href="admin_createuser.php">Create User</a></li>
             <li><a href="admin_edituser.php">Edit User</a></li>
             <li><a href="admin_deleteuser.php">Delete User</a></li>
-            <li><a href="scripts/caller.php?caller_id=logout">Sign Out</a></li>
             <li><a href="admin_addmovie.php">Add A Product</a></li>
             <li><a href="admin_deleteproduct.php">Delete A Product</a></li>
             <li><a href="admin_updateproduct.php">Edit A Product</a></li>
+            <li><a href="scripts/caller.php?caller_id=logout">Sign Out</a></li>
         </ul>
     </nav>
+
+</div>
 </body>
 </html>
